1de2617f26a18256e9a5c83202ca03f875866387
bilspatidar <96615671+bilspatidar@users.noreply.github.com>
2026-08-01 16:42:42 +0530

    refactor: import useMemo and remove gradle wrapper script

--- client/pages/admin/OrdersManager.jsx
+++ client/pages/admin/OrdersManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import * as XLSX from 'xlsx';
 import { useLanguage } from '../../context/LanguageContext';
 import { useData } from '../../context/DataContext';
